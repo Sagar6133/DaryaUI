c4ee435aa4c74a901b9790f6e4ff30fbf7d76b3e
sagar45 <sagar@grandhyper.com>
2021-04-02 14:26:46 +0530

    2nd commit

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:medicalapp/splash/splash_screen.dart';
 
 void main() {
-  runApp(MaterialApp(debugShowCheckedModeBanner: false, home: SplashScreen()));
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SplashScreen(),
+    ),
+  );
 }
-
--- lib/splash/splash_screen.dart
+++ lib/splash/splash_screen.dart
@@ -11,7 +11,7 @@ class SplashScreen extends StatefulWidget {
 
 class _Splash extends State<SplashScreen> {
   startTime() async {
-    var _duration = new Duration(seconds: 5);
+    var _duration = new Duration(seconds: 3);
     return new Timer(_duration, navigationPage);
   }
 
